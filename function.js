9671e4fe0f248da73db1c894c085171a0c15c275
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-06-21 18:07:02 +0530

    function

--- function.js
+++ function.js
@@ -1,14 +1,26 @@
 //Q.Find the factorial of a number
 
-function factorial(num){
+// function factorial(num){
   
     
-    if(num==0 || num==1){
-        return 1;
-    }else{
-        return num*factorial(num-1);
+//     if(num==0 || num==1){
+//         return 1;
+//     }else{
+//         return num*factorial(num-1);
+//     }
+// }
+// let num=5
+// ans=factorial(num);
+// console.log("The factorial of number " + num + " is " + ans)
+
+//Q. write a program to print all the digits of a number
+
+const number_of_digit=(num) => {
+    let count=0;
+    while(num!=0){
+        num=parseInt(num/10)
+        count++
     }
+    return count;
 }
-let num=5
-ans=factorial(num);
-console.log("The factorial of number " + num + " is " + ans)
+console.log(number_of_digit(1234))
