f527094a041be9178fa4559a962b5c91cc428c14
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-03-31 22:11:13 +0530

    practice

--- vasantkumarJs/index.js
+++ vasantkumarJs/index.js
@@ -1,17 +1,17 @@
-// console.log(fname)
-// var fname='vasant'
-// console.log(fname)
+console.log(fname)
+var fname='vasant'
+console.log(fname)
 
 
 
-// let a=null;
-// console.log(typeof(a))
-// let b=undefined;
-// console.log(typeof(b))
+let a=null;
+console.log(typeof(a))
+let b=undefined;
+console.log(typeof(b))
 
 
-// let a="null";
-// console.log(typeof(a))
-// let b="undefined";
-// console.log(typeof(b))
-// console.log(a+b);
+let x="null";
+console.log(typeof(a))
+let y="undefined";
+console.log(typeof(b))
+console.log(a+b);
